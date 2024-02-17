c00510280c7d729f6424a3fde9571543c5ded33c
ALR71 <alalr7105@oslomet.no>
2024-02-17 20:33:17 +0100

    Initializing Spring Boot-Project

---
+++ src/test/java/com/example/data1700oblig1_1/Data1700Oblig11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700oblig1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Oblig11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
